7193728253157edae5dabfa6145e3940f55eb606
vaithiyanathan <115526966+Vaithiyanathant@users.noreply.github.com>
2023-09-20 17:48:59 +0530

    Add files via upload

---
+++ ADSA _ASSIGN/index.js
@@ -0,0 +1,18 @@
+function nav(arr){
+    if (arr===1)
+    {
+        window.location.href = 'ass1.html';
+    }
+    else if(arr ===2)
+    {
+        window.location.href = 'ass2.html';
+    }
+    else if(arr ===3)
+    {
+        window.location.href = 'ass3.html';
+    }
+}
+
+function homepage(){
+    window.location.href = 'index.html';
+}
